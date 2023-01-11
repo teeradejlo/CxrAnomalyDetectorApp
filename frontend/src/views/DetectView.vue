6e5407215b4a9fa879f4398980c1829290145729
TeeradejLo <teeradejlo.2002@gmail.com>
2023-01-11 15:29:39 +0800

    fix detectview ui error
    error caused by trying to revert git repo

--- frontend/src/views/DetectView.vue
+++ frontend/src/views/DetectView.vue
@@ -1,6 +1,5 @@
 <script setup>
 import '../assets/common-classes.css'
-<<<<<<< HEAD
 import '../assets/common-keyframes.css'
 import { ref } from 'vue'
 import { axiosFastApi } from '../services/axios.js'
@@ -58,8 +57,6 @@ const detectImage = async () => {
 		alert('Please upload an image...')
 	}
 }
-=======
->>>>>>> parent of 138b54b (add detectview)
 </script>
 
 <script>
@@ -70,7 +67,6 @@ export default {
 
 <template>
 	<div class="detectview-container flexbox-center">
-<<<<<<< HEAD
 		<div v-if="!detectionStart" class="detectview-panel-1 d-flex">
 			<div class="detectview-uploadbox justify-content-center" :class="{ 'flexbox-center': imgSrc == null }">
 				<template v-if="imgSrc == null">
@@ -89,11 +85,10 @@ export default {
 			</div>
 		</div>
 		<div v-else class="detectview-panel-1 d-flex">
-			<img v-bind:src="imgSrc" />
+			<div class="detectview-uploadbox justify-content-center" :class="{ 'flexbox-center': imgSrc == null }">
+				<img class="img-auto-resize" v-bind:src="imgSrc" />
+			</div>
 		</div>
-=======
-
->>>>>>> parent of 138b54b (add detectview)
 	</div>
 </template>
 
@@ -102,5 +97,38 @@ export default {
 	flex-grow: 1;
 	overflow: hidden;
 	background-color: white;
+	animation: fade-in 0.5s ease 0s 1 normal backwards, translate-in-top 0.5s ease 0s 1 normal backwards;
+}
+
+.detectview-panel-1 {
+	background-color: white;
+	border-radius: 25px;
+	box-shadow: 0 0 50px rgb(221, 221, 221);
+	flex-direction: column;
+	padding: 30px;
+	max-width: 80%;
+	max-height: 80%;
+	width: 550px;
+}
+
+.detectview-uploadbox {
+	display: flex;
+	border: dashed 2px black;
+	border-radius: 15px;
+	margin-bottom: 30px;
+	height: 350px;
+	width: 100%;
+}
+
+.detectview-btn-panel button {
+	margin: 0 20px;
+}
+
+.detectview-btn-panel button:first-child {
+	margin-left: 0;
+}
+
+.detectview-btn-panel button:last-child {
+	margin-right: 0;
 }
 </style>
